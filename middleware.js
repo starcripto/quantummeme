2fde43d04faf52539107f9ba83fe0ec0e60e485f
eco-future <125764183+starcripto@users.noreply.github.com>
2025-03-14 00:37:45 -0300

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "d6f41fc3-7a77-440d-8aa8-f9bf0e0919ae");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
